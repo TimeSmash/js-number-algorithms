e40d51ac5fffff9dfb9871778a52b8c80bbbf114
TimeSmash <mcumm64@gmail.com>
2020-01-22 23:14:11 -0500

    Created file is-natural, contains algorithm to identify natural numbers (whole numbers 0 and above not including Infinity).

---
+++ is-natural.js
@@ -0,0 +1,27 @@
+function isPositive(num){
+	 if (num < 0){
+		return  false
+	} else { return true}
+}
+
+function isWholeNumber(num){
+    if (num.toString().includes(".")){
+       return false
+   } else { return true}
+}
+
+function isFinite(num){
+    if (num === Infinity){
+       return false
+   } else { return true}
+}
+
+
+function isNatural(num){
+	if(typeof num !== "number" || num === NaN){
+		return "Argument that is not a number received. Please input a number."
+    }
+	if (isPositive(num) && isWholeNumber(num) && isFinite(num)){
+        return true
+    } else {return false}
+}
