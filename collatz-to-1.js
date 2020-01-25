0a2b3e7d2c9929b73b8aace370ff10b78aea7abd
TimeSmash <mcumm64@gmail.com>
2020-01-24 21:47:27 -0500

    Made index.html to test out Chart.js usage, which is related to the collatz-to-1.js file. The collatz file takes a number and performs the Collatz process on it (if num=even, half it, if odd, num=num*3+1, repeat until num == 1), taking every number involved from the start to the end of the process.

---
+++ collatz-to-1.js
@@ -0,0 +1,22 @@
+// //From Wikipedia:
+// start with any positive integer n.
+//  Then each term is obtained from the previous term as follows: if the previous term is even, the next term is one half the previous term.
+//  If the previous term is odd, the next term is 3 times the previous term plus 1.
+//  The conjecture is that no matter what value of n, the sequence will always reach 1.
+
+function collatzTo1(num){
+	let terms = []
+    while(num >1){
+// 		console.log(num)
+		if(num % 2 ===0){
+			num = num/2
+			terms.push(num)
+        } else {
+			num = (3*num) + 1
+			terms.push(num)
+        }
+// 		debugger
+    }
+// 	console.log("final num", num)
+	return terms
+}
